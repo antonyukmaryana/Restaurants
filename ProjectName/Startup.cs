81b31ad4dd279b12f4fef4616c58f9141352af23
Student <example@example.com>
2019-04-30 08:19:22 -0700

    add more to template

--- ProjectName/Startup.cs
+++ ProjectName/Startup.cs
@@ -25,6 +25,8 @@ namespace ProjectName
 
     public void Configure(IApplicationBuilder app)
     {
+      app.UseDeveloperExceptionPage();
+      
       app.UseMvc(routes =>
       {
         routes.MapRoute(
